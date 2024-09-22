bdba5e2b41f446a8064e7124874b81e672ae856f
younes makhchan <younes.makhchan@oracle.com>
2024-09-22 20:41:50 +0100

    remove 1

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: 'CupKitten <hello@makhchan.com>',
+        from: 'CupKitten <hello@makhchan.tech>',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
